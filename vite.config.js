e7d3c9d442cc5cbbd19bc0a98a361914a16d2c82
Kostya <kostyabashkov@gmail.com>
2023-03-20 21:46:27 +0400

    inint

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import autoprefixer from 'autoprefixer';
+
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer
+      ],
+    },
+  },
+  root: 'src',
+  base: '/medtable/'
+});
