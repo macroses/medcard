0308aba66dc036e6cc0623138a7f64ad64103577
Kostya <kostyabashkov@gmail.com>
2023-03-30 21:37:45 +0400

    inint

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,8 @@ export default defineConfig({
         main: resolve(__dirname, 'src/index.html'),
         protocols: resolve(__dirname, 'src/protocols.html'),
         protocols_group: resolve(__dirname, 'src/protocols-group.html'),
+        appointments: resolve(__dirname, 'src/appointments.html'),
+        empty: resolve(__dirname, 'src/empty.html'),
       },
     },
   },
